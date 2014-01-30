df0e903d833049e12541c1ea21bed8159e4c8196
James Yang <james.yang92@gmail.com>
2014-01-30 17:56:23 -0500

    final touches to HW1

--- HW1/list.c
+++ HW1/list.c
@@ -37,7 +37,6 @@ void print_list(IntList *list) {
     return;
   }
   int len = length(list);
-  printf("list length: %d\n", len);
   Node *n = list->first;
   int it = 1;
   printf("[");
@@ -183,6 +182,7 @@ bool pop_front(IntList *list, int *ret) {
   return true;
 }
 
+// done
 bool del(IntList *list, int n, int *ret) {
   if (is_empty(list) || n < 0 || n >= length(list)) {
     *ret = -1;
@@ -190,19 +190,118 @@ bool del(IntList *list, int n, int *ret) {
   }
   Node *no = list->first;
   int counter = 0;
-  while (counter++ != n - 1)
+  if (n == 0)
+    return pop_front(list, ret);
+  while (counter != n - 1) {
     no = no->next;
+    counter++;
+  }
   Node *t = no->next;
   *ret = t->value;
   no->next = t->next;
+  free(t);
   return true;
 }
 
+
 IntList* append1(IntList *l1, IntList *l2) {
+  IntList *ret = mk_list();
+  ret = append2(ret, l1);
+  ret = append2(ret, l2);
+  return ret;
 }
 
+// done
 IntList* append2(IntList *l1, IntList *l2) {
+  if (is_empty(l1) && is_empty(l2))
+    return l1;
+    
+  Node *n2 = l2->first;
+
+  if (is_empty(l1)) {
+    push_back(l1, n2->value);
+    Node *t = (Node*)malloc(sizeof(Node));
+    t->value = n2->value;
+    t->next = NULL;
+    l1->first = t;
+    n2 = n2->next;
+  }
+  // if l1 is first empty
+  Node *n1 = l1->first;
+  while (n1->next != NULL)
+    n1 = n1->next;
+  while (n2 != NULL) {
+    Node *t = (Node*)malloc(sizeof(Node));
+    t->value = n2->value;
+    t->next = NULL;
+    n1->next = t;
+    n2 = n2->next;
+    n1 = n1->next;
+  }
+  
+  return l1;
 }
 
 IntList* append3(IntList *l1, IntList *l2) {
+  int ret;
+  while(pop_front(l2, &ret)) {
+    push_back(l1, ret);
+  }
+  return l1;
 }
+
+
+/*
+  1. Imagine that we have lists l1=[1,2,3] and l2=[4,5,6].
+     For each append function describe what values l1 and
+     l2 contain after execution of that function as well
+     as the contents of any other lists that might exist.
+
+     l3 = append1(l1, l2) -> l1=[1,2,3]
+                             l2=[4,5,6]
+			     l3=[1,2,3,4,5,6]
+
+     l1 = append2(l1, l2) -> l1=[1,2,3,4,5,6]
+                             l2=[4,5,6]
+
+     l1 = append3(l1, l2) -> l1=[1,2,3,4,5,6]
+                             l2=[]
+
+  2. What are the trade-offs between using the various
+     append functions? In particular how do they differ
+     in space usage and likelihood of memory safety
+     violations?
+
+     append1 has the worst space complexity. Memory
+     usage doubles, and both input linked lists remain
+     occupied. append1 is the safest to use because it
+     requires the declaration of a new linked list,
+     making it impossible to accidentally have the two
+     input linked list pointers from pointing to each
+     other or some unallocated space.
+
+     append2's space complexity grows linearly with l2.
+     It's not as bad as append1's space complexity, but
+     still wastes memory. It's easier to commit a
+     memory safety violation because the return value
+     is a pointer to the first linked list. Accidentally
+     setting l2 = append2(l1, l2) could lead to a seg-fault.
+     So could declaring a new pointer and setting it equal
+     to append2
+     i.e IntList *l3 = append2(l1, l2) -> seg-fault
+
+     append3's space complexity is constant, but suffers
+     from the same memory safety violations as append2.
+     
+  3. An alternative specification of append3 would involve
+     _node sharing_. Rather than transferring nodes from
+     l2 to l1, we could simply share the nodes so that l2
+     still points to its nodes. Why is this not a good design?
+     In particular, what kind of memory safety violation is
+     more likely to happen with this design?
+
+     l1 = append3(l1, l2);
+     free_list(l2);
+     print_list(l1); // SEGFAULT because l2 pointed to
+                     // nodes in l1
+ */
--- HW1/testlist.c
+++ HW1/testlist.c
@@ -2,28 +2,27 @@
 #include <stdio.h>
 
 int main(void) {
-  IntList *ll = mk_list();
-  push_back(ll, 6);
-  push_back(ll, 7);  
-  print_list(ll);
-  free_list(ll);
-  ll = mk_list();
-  push_back(ll, 1);
-  push_back(ll, 2);
-  push_back(ll, 3);
-  push_back(ll, 4);
-  push_front(ll, 5);
-  print_list(ll);
-  int idx;
-  index_of(ll, 6, &idx);
-  printf("%d %d\n", sum(ll), idx);
-  insert(ll, 10, 4);
-  print_list(ll);
-  intersparse(ll, 77);
-  int ret;
-  print_list(ll);
-  del(ll, 0, &ret);
-  print_list(ll);
+  IntList *l1 = mk_list();
+  IntList *l2 = mk_list();
 
+  push_back(l1, 1);
+  push_back(l1, 2);
+  push_back(l1, 3);
+  push_back(l2, 4);
+  push_back(l2, 5);
+  push_back(l2, 6);
+  print_list(l1);
+  print_list(l2);
+  printf("\n");
+  l1 = append3(l1, l2);
+  IntList *l3 = append1(l1, l2);
+  
+  print_list(l1);
+  print_list(l2);
+  print_list(l3);
+  
+  free_list(l1);
+  free_list(l2);
+  free_list(l3);
   return 0;
 }
